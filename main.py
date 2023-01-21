6ea62f9ec7d66ac41817c5b487cc0f3069c9f878
M.R.Nikhil <60879899+nikhilmr2000@users.noreply.github.com>
2023-01-21 23:37:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from flask import Flask, jsonify
 import os
+import sys
+
+import pickle
+import numpy as np
+import re
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
+ 
+@app.route('/predict',methods=["POST"])
+def hello_world():
+    ypred=""
+    l=[]
+    if request.method=="POST":
+        model = pickle.load(open("salary_predict.pkl", 'rb'))
+        content=request.get_json()
+        l.append(content["exp"])
+        ypred=model.predict(np.array(l).reshape(-1,1))[0]
+        ypred=stringconverter(str(ypred))
+    return ypred
+ 
+def stringconverter(name):
+    count=0
+    new_string=""
+    for i in range(0,len(name)):
+        if(name[i]=='.'):
+            count=1
+        if(count==0):
+           new_string+=name[i]  
+    return new_string
+    
+# main driver function
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
